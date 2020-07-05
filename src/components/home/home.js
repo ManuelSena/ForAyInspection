ddfbfae0362b1a4d74792db4b30c44c9b5dc74b7
ManuelS <manuelsena@me.com>
2020-07-04 23:28:14 -0700

    homepage

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,20 @@
 import * as React from "react";
 export const Home = () => {
-    return (React.createElement("div", null, "Home page is here"));
+    return (React.createElement("div", { className: "container fahome" },
+        React.createElement("div", { className: "container col-md-12" },
+            React.createElement("h1", null, "ForAy Inspection")),
+        React.createElement("div", { className: "col-md-8 container" },
+            React.createElement("h2", null, "Veteran Owned")),
+        React.createElement("div", { className: "col-md-8 container" },
+            React.createElement("h2", null, "Southern California"),
+            React.createElement("ul", null, "Greater Los Angeles Area")),
+        React.createElement("div", { className: "col-md-8 container" },
+            React.createElement("h2", null, "Certified Welding Inspector"),
+            React.createElement("ul", null, "License#: 1911311")),
+        React.createElement("div", { className: "contact col-md-8 container" },
+            React.createElement("h3", null, "Anthony Ramirez"),
+            React.createElement("a", { href: "mailto:Dispatch@ForAyInspection.com?subject=Contact" }, "Dispatch@ForAyInspection.com"),
+            React.createElement("ul", null,
+                React.createElement("a", { href: "tel:562-456-6001" }, "Phone: 562-456-6001")))));
 };
 //# sourceMappingURL=Home.js.map
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -3,7 +3,31 @@
 export const Home: React.StatelessComponent<{}> = () => {
 
     return (
-        <div>Home page is here</div>
+        <div className="container fahome">
+            <div className="container col-md-12">
+                <h1>ForAy Inspection</h1>
+            </div>
+
+            <div className="col-md-8 container">
+                <h2>Veteran Owned</h2>
+            </div>
+
+            <div className="col-md-8 container">
+                <h2>Southern California</h2>
+                <ul>Greater Los Angeles Area</ul>
+            </div>
+
+            <div className="col-md-8 container">
+                <h2>Certified Welding Inspector</h2>
+                <ul>License#: 1911311</ul>
+            </div>
+            <div className="contact col-md-8 container">
+                <h3>Anthony Ramirez</h3>
+                <a href="mailto:Dispatch@ForAyInspection.com?subject=Contact">Dispatch@ForAyInspection.com</a>
+                <ul><a href="tel:562-456-6001">Phone: 562-456-6001</a></ul>
+            </div>
+        </div>
+      
     );
 
 }
