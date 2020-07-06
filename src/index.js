392f90c37e6ba6f8adec7ec35012752fd390ae1d
ManuelS <manuelsena@me.com>
2020-07-06 01:11:01 -0700

    gg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppRouter } from "../src/router";
-import './css/site.css';
 ReactDOM.render(React.createElement(AppRouter, null), document.getElementById("root"));
 //# sourceMappingURL=index.js.map
